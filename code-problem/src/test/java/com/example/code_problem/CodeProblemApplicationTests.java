e5c3fadfa0aa551909741979994196c9521443d9
Rishabh <rishabhptl2003@gmail.com>
2025-04-10 23:58:00 +0530

    Initial Commit - Code Problem Service

---
+++ code-problem/src/test/java/com/example/code_problem/CodeProblemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.code_problem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeProblemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/main/java/com/interviewX/code_problem/CodeProblemApplication.java
@@ -0,0 +1,13 @@
+package com.interviewX.code_problem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeProblemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeProblemApplication.class, args);
+	}
+
+}
